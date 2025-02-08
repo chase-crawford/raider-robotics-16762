e226074e525e94d5249b6eb5f68157af3799458a
Kuillotine <catherineshea25@gmail.com>
2025-02-07 23:53:16 -0500

    Wrote new path for ComplexRight - pushes the sample into the obsv. zone for human player to turn into specimens, then bot grabs them, places on sub, and park at lv1 ascent. Not been physically tested at time of commit

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -93,76 +93,83 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-9, 63, Math.toRadians(-90)))
                         // set up lift to rise to upper sub rung
-                        //.stopAndAdd(new RaiseElbow())
-                        //.stopAndAdd(new SendLiftTo(1470))
 
                         // move to be in front of sub and wait for lift to raise
-                        .splineTo(new Vector2d(-9, 43), Math.toRadians(-90))
-                        //.stopAndAdd(new WaitForLift())
+                            .splineTo(new Vector2d(-9, 43), Math.toRadians(-90))
 
                         // place specimen on rung
-                        //.stopAndAdd(new SendLiftTo(770))
-                        //.stopAndAdd(new WaitForLift())
-                        //.stopAndAdd(new OpenClaw())
 
                         // move lift down and back up
-                        //.stopAndAdd(new SendLiftTo(InitVars.SPECIMEN_PRESET))
-                        .lineTo(new Vector2d(-9,48))
+                            //.lineTo(new Vector2d(-9,48))
 
-                        // grab first specimen from the wall
-                        .splineToLinearHeading(new Pose2d(-9, 51, Math.toRadians(90)), Math.toRadians(45))
-                        //.stopAndAdd(new LowerElbow())
-                        .lineTo(new Vector2d(-48, 51))
-                        ////.stopAndAdd(new SendLiftTo(InitVars.VIPER_HOME))
-                        //.stopAndAdd(new CloseClaw())
-                        .waitSeconds(0.5)
+                        //Move infront of first sample to push into obsv. zone
+                            .lineTo(new Vector2d(-28, 42))
+                            .splineToConstantHeading(new Vector2d(-45, 10), Math.PI)
+                            .lineTo(new Vector2d(-46, 10))
+
+                        //Push sample into obsv. zone
+                            .lineTo(new Vector2d(-46,60))
+
+                        //Move infront of the second sample to push into obsv. zone
+                            .lineTo(new Vector2d(-46, 10))
+                            .lineTo(new Vector2d(-58, 10))
+
+                        //Push sample into obsv. zone
+                            .lineTo(new Vector2d(-58, 60))
+
+                        //Spin to grab specimen from wall
+                            .splineToLinearHeading(new Pose2d(-46, 55, Math.toRadians(90)), Math.PI)
+                            .waitSeconds(0.5)
+
+                        //Place specimen on top rung on submersible and go back to grab second specimen
+                            .splineToLinearHeading(new Pose2d(-6, 43, Math.toRadians(-90)), Math.PI/2)
+                            .lineTo(new Vector2d(-6, 60))
+                            .splineToLinearHeading(new Pose2d(-58, 58, Math.toRadians(90)), Math.PI)
+                            .waitSeconds(0.5)
+
+                        //Place second specimen on top rung of submersible
+                            .lineTo(new Vector2d(-34, 50))
+                            .splineToLinearHeading(new Pose2d(-3, 43, Math.toRadians(-90)), Math.PI/2)      //try to make angle better :/
+
+                        //End at lv 1 ascent
+                            .lineTo(new Vector2d(-46, 43))
+                            .splineToLinearHeading(new Pose2d(-34, 5, Math.toRadians(0)), Math.PI)
+                            .lineTo(new Vector2d(-25,12))
+
+                        /* grab first specimen from the wall
+                            .splineToLinearHeading(new Pose2d(-9, 51, Math.toRadians(90)), Math.toRadians(45))
+                            .lineTo(new Vector2d(-48, 51))
+                            .waitSeconds(0.5)
 
                         // move back to rung
-                        //.stopAndAdd(new RaiseElbow())
-                        //.stopAndAdd(new SendLiftTo(InitVars.MID_PRESET))
-                        .waitSeconds(ELBOW_WAIT)
-                        .splineToLinearHeading(new Pose2d(-9, 51, Math.toRadians(-90)), 45)
-                        .splineToConstantHeading(new Vector2d(-6, 43.25), Math.toRadians(-45))
+                            .waitSeconds(ELBOW_WAIT)
+                            .splineToLinearHeading(new Pose2d(-9, 51, Math.toRadians(-90)), 45)
+                            .splineToConstantHeading(new Vector2d(-6, 43.25), Math.toRadians(-45))
 
                         // hang specimen
-                        //.stopAndAdd(new WaitForLift())
-                        //.stopAndAdd(new SendLiftTo(770))
-                        //.stopAndAdd(new WaitForLift())
-                        //.stopAndAdd(new OpenClaw())
 
                         // move lift down and back up
-                        //.stopAndAdd(new SendLiftTo(InitVars.SPECIMEN_PRESET))
-                        .lineTo(new Vector2d(-9,48))
+                            .lineTo(new Vector2d(-9,48))
 
                         // grab first specimen from the wall
-                        .splineToLinearHeading(new Pose2d(-9, 51, Math.toRadians(90)), Math.toRadians(45))
-                        //.stopAndAdd(new LowerElbow())
-                        .lineTo(new Vector2d(-48, 51))
-                        ////.stopAndAdd(new SendLiftTo(InitVars.VIPER_HOME))
-                        //.stopAndAdd(new CloseClaw())
-                        .waitSeconds(0.5)
+                            .splineToLinearHeading(new Pose2d(-9, 51, Math.toRadians(90)), Math.toRadians(45))
+                            .lineTo(new Vector2d(-48, 51))
+                            .waitSeconds(0.5)
 
                         // move back to rung
-                        //.stopAndAdd(new RaiseElbow())
-                        //.stopAndAdd(new SendLiftTo(InitVars.MID_PRESET))
-                        .splineToLinearHeading(new Pose2d(-9, 51, Math.toRadians(-90)), 45)
-                        .splineToConstantHeading(new Vector2d(-3, 43.25), Math.toRadians(-45))
+                            .splineToLinearHeading(new Pose2d(-9, 51, Math.toRadians(-90)), 45)
+                            .splineToConstantHeading(new Vector2d(-3, 43.25), Math.toRadians(-45))
 
-                        // hang specimen
-                        //.stopAndAdd(new WaitForLift())
-                        //.stopAndAdd(new SendLiftTo(770))
-                        //.stopAndAdd(new WaitForLift())
-                        //.stopAndAdd(new OpenClaw())
-                        //.stopAndAdd(new SendLiftTo(InitVars.BOTTOM_PRESET))
+                        // hang specimen*/
 
                         //End auto path and build
-                        .build());
+                            .build());
 
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
-                .setDarkMode(true)
-                .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
-                .start();
+            .setDarkMode(true)
+            .setBackgroundAlpha(0.95f)
+            .addEntity(myBot)
+            .start();
     }
 }
